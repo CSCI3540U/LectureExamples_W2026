42ac5a2465ab27dc8d871367c15e4885d71444cd
randyfortier <randy.fortier@uoit.ca>
2026-01-20 10:29:09 -0500

    Fixed web app issues - which were caused by a locked up virtual machine

--- 02_web_apps/basic_web.js
+++ 02_web_apps/basic_web.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 9000;
+const cookieParser = require('cookie-parser');
 
 // middleware
 app.use(express.static('static'));
 app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+let lastSessionId = 1000000;
+
+let sessionData = {};
 
 // routes
 app.get('/index', (request, response) => {
-    response.send('<h1>index page</h1>');
+    const sessId = request.cookies['session-id'];
+    response.send(`<h1>index page (Session ID: ${sessId})</h1>`);
 });
 
 app.get('/login', (request, response) => {
@@ -17,8 +24,19 @@ app.get('/login', (request, response) => {
 });
 
 app.post('/login', (request, response) => {
-    console.log(request.body);
-    response.send(`/login POST ${request.body['username']}`);
+    // console.log(request.body);
+    // response.send(`/login POST ${request.body['username']}`);
+    const sessId = `${lastSessionId++}`;
+
+    sessionData[sessId] = request.body.username;
+
+    response.cookie('session-id', sessId, {
+        /*secure: true,*/
+        httpOnly: true,
+        path: '/'
+    });
+
+    response.redirect('/index');
 });
 
 app.get('/posts/:category/:postid', (request, response) => {
